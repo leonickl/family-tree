337b75fc9d3a7a4805ad52b20697c5d126c14855
Leo Nickl <leol@leonickl.de>
2025-12-28 21:15:25 +0100

    show all families of a person

--- views/tree.php
+++ views/tree.php
@@ -1,7 +1,18 @@
 <h1><em><?= $start ?></em> is member in these families</h1>
 
-<div class="column">
+<div class="column-4 items-center">
     <?php foreach($start->families() as $family): ?>
-        <div><?= $family ?></div>
+        <div class="column items-center">
+            <div class="row">
+                <div class="border px-1 py-05"><?= lnk()->tree($family->husband()) ?></div>
+                <div class="border px-1 py-05"><?= lnk()->tree($family->wife()) ?></div>
+            </div>
+
+            <div class ="row">
+                <?php foreach($family->children() as $child): ?>
+                    <div class="border px-1 py-05"><?= lnk()->tree($child) ?></div>
+                <?php endforeach ?>
+            </div>
+        </div>
     <?php endforeach ?>
 </div>
