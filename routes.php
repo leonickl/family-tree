d2fa80c067fc3e12bc326e02d519839316b1f4b0
Leo Nickl <leol@leonickl.de>
2025-12-28 13:03:44 +0100

    add css

--- routes.php
+++ routes.php
@@ -4,3 +4,4 @@ use App\Controllers\ExampleController;
 use PXP\Core\Lib\Route;
 
 Route::get('/')->do(ExampleController::class, 'index');
+Route::get('/css/{file}')->do(AssetController::class, 'css');
