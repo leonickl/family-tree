3fa95c55a22a676f6a6e7f7aa38f98a16c206ef7
Leo Nickl <leol@leonickl.de>
2025-12-28 12:09:34 +0100

    fis tree path

--- src/Types/Tree.php
+++ src/Types/Tree.php
@@ -15,7 +15,7 @@ class Tree
     public static function init(string $file = 'tree')
     {
         $parsed = new \Gedcom\Parser()
-            ->parse(storage_path('trees/'.$file.'.ged'));
+            ->parse(path('database/trees/'.$file.'.ged'));
 
         return self::$instance = new self($parsed);
     }
