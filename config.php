128968325881a25f95bc197da3eaead08ee4bad8
Leo Nickl <leol@leonickl.de>
2025-12-29 06:08:14 +0100

    enable auth

--- config.php
+++ config.php
@@ -1,7 +1,8 @@
 <?php
 
 return [
-    'title' => 'PXP App',
+    'title' => 'Family Tree',
     'domain' => 'localhost',
     'port' => 8085,
+    'credentials' => json_decode(env('CREDENTIALS')),
 ];
