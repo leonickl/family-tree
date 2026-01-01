2c3a16a6850f3d202c0c96a3e72638f793fde15b
Leo Nickl <leol@leonickl.de>
2026-01-01 18:48:57 +0100

    inline html for person

--- src/Plot/Person.php
+++ src/Plot/Person.php
@@ -8,10 +8,17 @@ class Person
 
     public function __toString()
     {
-        return view('person', [
-            'person' => $this->person,
-            'highlight' => $this->highlight,
-            'area' => [$this->y, $this->x, $this->y + 1, $this->x + 1],
-        ]);
+        $area = implode(' / ', [$this->y, $this->x, $this->y + 1, $this->x + 1]);
+
+        $classes = 'px-1 py-05 rounded text-center-both';
+        $styles = "grid-area: $area; z-index: 10";
+
+        if ($this->highlight) {
+            return "<div class=\"$classes bg-primary\" style=\"$styles; width: 10rem; color: #f9fafb\">$this->person</div>";
+        }
+
+        $link = lnk()->tree($this->person);
+
+        return "<div class=\"$classes border\" style=\"$styles; background: var(--main-background); width: 10rem\">$link</div>";
     }
 }
