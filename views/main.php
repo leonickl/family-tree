fb6853e8b06e6e30f62ac3ff89806d79ec2c18a4
Leo Nickl <leol@leonickl.de>
2026-01-01 16:18:01 +0100

    add tree list label

--- views/main.php
+++ views/main.php
@@ -1,5 +1,7 @@
 <h1>Welcome to Family Tree</h1>
 
+<p>You have access to these trees:</p>
+
 <?php foreach($trees as $tree): ?>
     <p><a href="/trees/<?= $tree ?>"><?= $tree ?></a></p>
 <?php endforeach ?>
